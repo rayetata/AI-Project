8db194a439606e778c199edc8144040dc68c8581
Ashley-Nicole18 <ashleynicole.gellado-23@cpu.edu.ph>
2025-09-30 11:48:12 +0800

    feat: Added new feature 'instruction'

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/test', (req, res) => {
 });
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
 app.post("/ai", async (req, res) => {
     const { prompt } = req.body;
